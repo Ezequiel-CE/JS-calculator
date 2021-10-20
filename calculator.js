902ab78dfc90c7f55704f8b1b9cd024596d16475
Ezequiel carrizo <ezequielce490@gmail.com>
2021-10-20 13:52:36 -0300

    added conexions to buttons

--- calculator.js
+++ calculator.js
@@ -35,7 +35,7 @@ let labelContent = "";
 const addToScreen = (e) => {
   num = e.target.textContent;
   labelContent += num;
-  displayCurrent.textContent = labelContent;
+  displayCurrent.textContent = labelContent.substring(0, 15);
 };
 
 btnNumbers.forEach((btn) => {
@@ -44,25 +44,71 @@ btnNumbers.forEach((btn) => {
 
 //operations
 
-let value1 = false;
+let value1 = 0;
+let operation = "";
 
-//sum
+//elementos
 const btnSum = document.querySelector(".btn-sum");
+const btnEqual = document.querySelector(".btn-result");
+const btnSubstract = document.querySelector(".btn-sub");
+const btnMulti = document.querySelector(".btn-multi");
+const btnDivide = document.querySelector(".btn-divide");
 
-const pressSum = () => {
-  console.log("hello");
+//apretar una operacion
 
-  if (value1 === false) {
-    value1 = parseInt(labelContent);
-    labelContent = "";
-    return;
+const pressOperation = (operator) => {
+  //si apretamos un btn de operacion por segunda vez nos el calculo
+  if (operation !== "") {
+    pressEqual(operator);
   }
 
-  let actualNum = parseInt(labelContent);
-  let sum = operate("+", value1, actualNum);
-  labelContent = sum;
-  displayCurrent.textContent = sum;
-  value1 = sum;
+  //guarda el valor ,da la operacion y setea el label para escribir un nuevo numeor
+  value1 = parseInt(displayCurrent.textContent);
+  operation = operator;
+  labelContent = "";
+  console.log(operation);
 };
 
-btnSum.addEventListener("click", pressSum);
+//result
+
+const pressEqual = () => {
+  //guara el numero actual en el display para usarlo como valor 2
+  const currentNum = parseInt(displayCurrent.textContent);
+
+  //verifica que opercaion hay actualmente
+  if (operation === "+") {
+    const sum = operate("+", value1, currentNum);
+    displayCurrent.textContent = sum;
+    operation = "";
+  }
+  if (operation === "-") {
+    const sub = operate("-", value1, currentNum);
+    displayCurrent.textContent = sub;
+    operation = "";
+  }
+  if (operation === "*") {
+    const multi = operate("*", value1, currentNum);
+    displayCurrent.textContent = multi;
+    operation = "";
+  }
+  if (operation === "/") {
+    //caso de division de 0
+    if (currentNum === 0) {
+      displayCurrent.textContent = "cant divide by 0";
+      operation = "";
+      labelContent = "";
+      return;
+    }
+    const divi = operate("/", value1, currentNum).toString();
+    displayCurrent.textContent = divi.substring(0, 15);
+    operation = "";
+  }
+};
+
+//uso una callback anonima para poder llamar a otra con argumentos
+
+btnSum.addEventListener("click", () => pressOperation("+"));
+btnSubstract.addEventListener("click", () => pressOperation("-"));
+btnMulti.addEventListener("click", () => pressOperation("*"));
+btnDivide.addEventListener("click", () => pressOperation("/"));
+btnEqual.addEventListener("click", pressEqual);
